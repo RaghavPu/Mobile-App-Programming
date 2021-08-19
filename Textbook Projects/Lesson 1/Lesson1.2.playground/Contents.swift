ec8725388eb0f34a7c938fb08924857b93eee1ce
RaghavPu <46982251+RaghavPu@users.noreply.github.com>
2021-08-19 09:18:46 -0500

    Lesson 1.2
    Lesson 1.2

--- Textbook Projects/Lesson 1/Lesson1.2.playground/Contents.swift
+++ Textbook Projects/Lesson 1/Lesson1.2.playground/Contents.swift
@@ -46,3 +46,59 @@ aPerson.sayHello()
 anotherPerson.sayHello()
 
 
+
+//******* TYPE SAFETY *********//
+let playerName = "Julian"
+var playerScore = 1000
+var gameOver = false
+
+// Type Safety feature will not allow you to change types after declaration
+//playerScore = playerName
+
+var wholeNumber = 30
+var numberWithDecimals = 17.5
+
+// Type Safety also applies between double values and integer values
+//wholeNumber = numberWithDecimals
+
+
+//**** NUMBER TRICK *****//
+var largeUglyNumber = 1000000000
+var largePrettyNumber = 1_000_000_000
+
+
+
+//******* TYPE INFERENCE *******//
+let cityName = "San Francisco"
+// “San Francisco” is obviously a `String`, so the compiler automatically assigns the type of cityName to a `String`.
+let cityName2: String = "San Francisco"
+
+let pi = 3.1415927
+// 3.1415927 is a number with decimal points, so the compiler automatically assigns the type `pi` to a `Double`.
+let pi2: Double = 3.1415927
+
+let number: Double = 3
+print(number)
+
+
+//****** WAYS TO USE TYPE *******//
+let firstName: String
+firstName = "Layne"
+
+let middleInitial: Character = "R"
+
+struct Car {
+    var make: String
+    var model: String
+    var year: Int
+}
+let myCar: Car
+
+
+/*
+ You cannot declare a variable but not initialize it, if it does not have a type
+ For example: var x   --> doesn't work |||||| var x: Int    --> proper way to do it
+ 
+ You cannot print a variable that is initialized
+ */
+
