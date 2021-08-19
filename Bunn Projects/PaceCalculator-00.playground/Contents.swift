ef897af7771ef8277f37d7d1e4ac9b5b44482e6b
raghavPu <raghavsrisurya@gmail.com>
2021-08-19 10:33:36 -0500

    Pace Calculator
    Pace Calculator

--- Bunn Projects/PaceCalculator-00.playground/Contents.swift
+++ Bunn Projects/PaceCalculator-00.playground/Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
-var str = "Hello, playground"
+// Step 1
+let raceDistance: Double = 5
+var timeElapsed: Double = 6.25
+var distTravelled: Double = 1
+
+
+// Step 2
+let pace = distTravelled / timeElapsed
+let remainingTime = (raceDistance - distTravelled) / pace
+let totalTime = remainingTime + timeElapsed;
+
+// Step 3
+print("At the current pace, it will take an additional \(remainingTime) minutes to finish the race.")
+print("At the current pace, the total time will be \(totalTime) minutes for the \(raceDistance) kilometers.")
+
