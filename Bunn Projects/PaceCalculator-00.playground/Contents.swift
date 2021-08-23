9c25a1231dea7ad31ba68b993a3edf93ec2a45e0
RaghavPu <46982251+RaghavPu@users.noreply.github.com>
2021-08-23 10:23:43 -0500

    Finish rest of Pace Calculator

--- Bunn Projects/PaceCalculator-00.playground/Contents.swift
+++ Bunn Projects/PaceCalculator-00.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+//******** PACE CALCULATOR *******//
+
 // Step 1
 let raceDistance: Double = 5
 var timeElapsed: Double = 6.25
@@ -14,4 +16,59 @@ let totalTime = remainingTime + timeElapsed;
 // Step 3
 print("At the current pace, it will take an additional \(remainingTime) minutes to finish the race.")
 print("At the current pace, the total time will be \(totalTime) minutes for the \(raceDistance) kilometers.")
+print("\n")
+
+
+
+//******** GPA PLANNING CALCULATOR *******//
+var currentGPA = 2.8
+let targetGPA = 3.0
+var currentCredits = 25
+let additionalCredits = 15
+
+var currTotal = currentGPA * 25
+var targetTotal = Double(additionalCredits + currentCredits) * targetGPA
+var remainingTotal = targetTotal - currTotal
+var gpaForAdditionalCredits = remainingTotal / Double(additionalCredits)
+
+print("To achieve a target GPA of \(targetGPA), the GPA for the next \(additionalCredits) credits needs to be \(gpaForAdditionalCredits) or higher.")
+print("\n")
+
+
+
+
+//******** RESTAURANT CHECK CALCULATOR *******//
+var subTotal: Double = 150
+var serviceDescription = "excellent"
+
+var discount: Double
+if subTotal <= 50 {
+    discount = 10
+} else if subTotal < 100 {
+    discount = 20
+} else {
+    discount = subTotal * 0.3
+}
+
+var tipPercentage: Double
+if serviceDescription == "bad" {
+    tipPercentage = 0
+} else if serviceDescription == "so-so" {
+    tipPercentage = 0.1
+} else if serviceDescription == "good" {
+    tipPercentage = 0.15
+} else if serviceDescription == "excellent" {
+    tipPercentage = 0.2
+} else if serviceDescription == "superior" {
+    tipPercentage = 0.25
+} else {
+    tipPercentage = 0
+}
+
+var total = (subTotal - discount) * (1 + tipPercentage)
+
+print("Subtotal: \(subTotal)")
+print("Discount: \(discount)")
+print("Tip: \(tipPercentage * 100)%")
+print("Your order total is \(total).")
 
