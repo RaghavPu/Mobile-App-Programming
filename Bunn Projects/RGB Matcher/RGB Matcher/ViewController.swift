79e636d411f1aac36e4524855f523f91959d4d29
RaghavPu <46982251+RaghavPu@users.noreply.github.com>
2021-10-04 10:35:06 -0500

    RGB Matcher Final

--- Bunn Projects/RGB Matcher/RGB Matcher/ViewController.swift
+++ Bunn Projects/RGB Matcher/RGB Matcher/ViewController.swift
@@ -26,8 +26,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        
+        initializeUI()
         resetGame()
     }
     
